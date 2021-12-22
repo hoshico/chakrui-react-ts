d14953db52c0e2b744ff2f8e8a931b5dd50ee314
hoshico <melancholinista.yasu@gmail.com>
2021-12-22 14:31:58 +0900

    add MenuIconButton.tsx

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,6 +1,6 @@
 import { Box, Button, Drawer, DrawerBody, DrawerContent, DrawerOverlay, Flex, Heading, IconButton, Link, useDisclosure } from "@chakra-ui/react";
 import { memo, VFC } from "react";
-import { HamburgerIcon } from "@chakra-ui/icons";
+import { MenuIconButton } from "../../atoms/button/MenuIconButton";
 
 export const Header: VFC = memo(() => {
     const { isOpen, onOpen, onClose } = useDisclosure();
@@ -29,14 +29,7 @@ export const Header: VFC = memo(() => {
                 </Box>
                     <Link>設定</Link>
             </Flex>
-            <IconButton 
-              aria-label="メニューボタン"  
-              icon={<HamburgerIcon />} 
-              size="sm" 
-              variant="unstyled" 
-              display={{ base: "block", md: "none"}} 
-              onClick={onOpen}
-              />
+            <MenuIconButton onOpen={onOpen} />
         </Flex>
         <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
             <DrawerOverlay>
