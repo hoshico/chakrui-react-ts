3f23473987f8c39fe839ba60f0fc6fa81fb32297
hoshico <melancholinista.yasu@gmail.com>
2021-12-27 06:21:36 +0900

    add usemessage.ts

--- src/router/HomeRoutes.tsx
+++ src/router/HomeRoutes.tsx
@@ -1,4 +1,4 @@
-import { Home } from "../components/pages/Home ";
+import { Home } from "../components/pages/Home";
 import { Page404 } from "../components/pages/Page404";
 import { Setting } from "../components/pages/Setting";
 import { UserManagement } from "../components/pages/UserManagement";
