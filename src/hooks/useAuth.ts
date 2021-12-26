03be35c7aefece23d536474929b717773b763e77
hoshico <melancholinista.yasu@gmail.com>
2021-12-27 06:26:50 +0900

    update useAuth.ts

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,9 +3,11 @@ import axios from "axios"
 import { useHistory } from "react-router-dom"
 
 import { User } from "../types/api/user"
+import { useMessage } from "./useMessage"
 
 export const useAuth = () => {
-    const history = useHistory()
+    const history = useHistory();
+    const { showMessage } = useMessage();
 
     const [loading, setLoading] = useState(false);
 
@@ -17,15 +19,16 @@ export const useAuth = () => {
             .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
             .then((res) => {
                 if (res.data) {
-                history.push("/home");
+                    showMessage({ title: "ログインしました", status: "success"})
+                    history.push("/home");
                 } else {
-                alert("ユーザーが見つかりません");
+                    showMessage({ title: "ユーザーが見つかりません", status: "error"})
                 }
             })
-            .catch(() => alert("ログインできません"))
+            .catch(() => showMessage({ title: "ログイン出来ません", status: "error"}))
             .finally(() => setLoading(false));
         }, 
-        [history]
+        [history, showMessage]
     );
 
     return { login, loading }
